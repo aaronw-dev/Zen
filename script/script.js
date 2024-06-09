924ba874e4ccb0c868b70d45ac4a62e4f3ba8ebc
AW_Dev <riptidegamesceo@gmail.com>
2024-06-08 21:45:43 -0700

    added good navigation

--- script/script.js
+++ script/script.js
@@ -1,24 +1,31 @@
 var openPanels = []
-function openPanel(id) {
+const bottomNav = document.getElementById("bottomnav")
+function openPanel(id, cangoback = true) {
     let targetPanel = document.getElementById(id)
     if (targetPanel != null) {
         if (!targetPanel.className.split(" ").includes("open")) {
             targetPanel.style.zIndex = 100;
             targetPanel.classList.add("open")
-            openPanels.push(targetPanel)
-            console.log("e")
+            if (cangoback)
+                openPanels.push(targetPanel)
+            setNavOpen(openPanels.length == 0)
         }
     } else {
         throw new Error("That panel doesn't exist!")
     }
+    console.log(openPanels.length)
 }
-
+function setNavOpen(bool) {
+    bool ? bottomNav.classList.remove("closed") : bottomNav.classList.add("closed")
+}
+function closeCurrentPanel() {
+    if (openPanels.length > 0) {
+        openPanels.at(-1).classList.remove("open")
+        openPanels.pop()
+        setNavOpen(openPanels.length == 0)
+    }
+}
 document.addEventListener('swiped-left', function (e) {
-    console.log(e.target);
-    console.log(e.detail);
 });
 
-document.addEventListener('swiped-right', function (e) {
-    console.log(openPanels)
-    openPanels.at(-1).classList.remove("open")
-});
+document.addEventListener('swiped-right', function (e) { closeCurrentPanel() });
