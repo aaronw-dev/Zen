d9d3bdf153c2d8251dc86d08c020116da728b663
= <riptidegamesceo@gmail.com>
2024-06-17 09:41:09 -0700

    added saving title

--- script/journal.js
+++ script/journal.js
@@ -1,4 +1,5 @@
 const journalDateText = document.getElementById("journaldate")
+const journalTitle = document.getElementById("journal-title")
 const options = {
     weekday: 'long',
     year: 'numeric',
@@ -8,3 +9,9 @@ const options = {
 
 let now = new Date()
 journalDateText.innerHTML = now.toLocaleDateString(undefined, options) + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + now.toLocaleTimeString();
+var currentJournalName = localStorage.getItem("currentJournal") != null ? localStorage.getItem("currentJournal"):"test"
+journalTitle.value = currentJournalName
+journalTitle.addEventListener("input", (e)=>{
+    currentJournalName = journalTitle.value
+    localStorage.setItem("currentJournal",currentJournalName)
+})
